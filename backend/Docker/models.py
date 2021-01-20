56796a8a299610a5a6a54458ff6f99c45d50a6ec
scyoon <scyoon@stealien.com>
2021-01-20 18:55:43 +0900

    [Edit] Docker 모델 proejct field Foriegn Key로 변경

--- backend/Docker/models.py
+++ backend/Docker/models.py
@@ -6,12 +6,12 @@ class Project(models.Model):
 
 
 class Docker(models.Model):
-    project = models.OneToOneField(
+    project = models.ForeignKey(
         Project,
         on_delete=models.CASCADE
     )
     name = models.CharField(max_length=128)
     image = models.CharField(max_length=64, null=True, blank=True)
     environment = models.TextField(default=None, null=True, blank=True)
-    ports = models.TextField(default=None, null=True, blank=True)
+    ports = models.TextField(default=None, null=True, blank=True)   # store data as json
 
