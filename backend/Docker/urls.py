4704663a7768781c8042c35220f73fb67f0ae897
scyoon <scyoon@stealien.com>
2021-01-20 18:56:43 +0900

    [Edit] project_id 만으로 docker-compose.yml 파일 자동으로 작성해주는 코드 추가

--- backend/Docker/urls.py
+++ backend/Docker/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import create_project
+from .views import create_project, create_docker_compose_yml
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('project/create', create_project),
+    path('project/<int:project_id>/docker-compose.yml', create_docker_compose_yml),
 ]
--- backend/Docker/views.py
+++ backend/Docker/views.py
@@ -1,7 +1,11 @@
 # Django
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
-from .models import Docker
+from .models import Project, Docker
+import Lib.api
+
+import yaml
+import json
 
 
 def create_project(request):
@@ -11,6 +15,46 @@ def create_project(request):
     :return:
     """
     return HttpResponse('fuck')
+
+
+def create_docker_compose_yml(request, project_id):
+    """
+    Create docker-compose.yml
+    """
+    project = Project.objects.get(id=project_id)
+    if not project:
+        # No such project
+        return api.fail('No such project')
+
+    docker_compose = {'version': 3, 'services': {}}
+    dockers = Docker.objects.filter(project=project)
+    for docker in dockers:
+        local_docker_compose = {}
+        local_docker_compose['image'] = docker.image
+
+        # Parse environment stored in JSON
+        environments = json.loads(docker.environment)
+        local_docker_compose['environment'] = \
+            [f"{key}={value}" for key, value in environments.items()]
+
+        # Parse ports stored in JSON
+        """
+        # ports will be ...
+        [
+            {outer: 80, inner: 8080},
+            {outer: 3306, inner: 1234},
+        ]
+        """
+        ports = json.loads(docker.ports)
+        local_docker_compose['ports'] = [f"{port.outer}:{port.inner}" for port in ports]
+
+        docker_compose[docker.name] = local_docker_compose
+
+    docker_compose_yml = yaml.dump(docker_compose)
+
+    # TODO: store docker-compose.yml as real file
+
+    return HttpResponse(docker_compose_yml)
 
 
 def create_docker_compose_yml(request):
