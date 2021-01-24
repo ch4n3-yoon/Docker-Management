bc27c3f54720e3b4219e4b3bf06fa585d1f8a0a1
ch4n3-yoon <ch4n3.yoon@gmail.com>
2021-01-24 23:30:32 +0900

    [Edit] 프론트엔드 대략적 구성

---
+++ frontend/src/pages/docker-compose.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+const DockerCompose = () => {
+    return <Container>
+        <div>
+            test
+        </div>
+    </Container>
+}
+
+export default DockerCompose;
